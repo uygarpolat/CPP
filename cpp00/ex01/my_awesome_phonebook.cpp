9fe175126c3c77b1fd2cde3308c83783de24d59a
Uygar Polat <upolat@c1r6p11.hive.fi>
2024-08-29 15:58:14 +0300

    files

--- cpp00/ex01/my_awesome_phonebook.cpp
+++ cpp00/ex01/my_awesome_phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 12:30:17 by upolat            #+#    #+#             */
-/*   Updated: 2024/08/29 10:35:52 by upolat           ###   ########.fr       */
+/*   Updated: 2024/08/29 15:37:44 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <iomanip>
 #include <string>
 #include <sstream>
+#include <limits>
 
 class Contact
 {
@@ -56,22 +57,28 @@ void	ft_cout(std::string input, int i)
 		std::cout << std::endl;
 }
 
+void getInput(const std::string& prompt, std::string& inputField)
+{
+	while (inputField.empty())
+	{
+        std::cout << prompt;
+        std::getline(std::cin, inputField);
+        if (inputField.empty())
+            std::cout << "Input cannot be empty. Please try again.\n";
+    }
+}
+
 
 void	ft_add(class PhoneBook *phonebook)
 {
 
 	// Makes sure to ignore newline characters from previous reads.
 	//std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	std::cout << "Enter first name: ";
-	std::getline(std::cin, phonebook->contact[phonebook->counter % 8].FirstName);
-	std::cout << "Enter last name: ";
-	std::getline(std::cin, phonebook->contact[phonebook->counter % 8].LastName);
-	std::cout << "Enter nickname: ";
-	std::getline(std::cin, phonebook->contact[phonebook->counter % 8].Nickname);
-	std::cout << "Enter phone number: ";
-	std::getline(std::cin, phonebook->contact[phonebook->counter % 8].PhoneNumber);
-	std::cout << "Enter darkest secret: ";
-	std::getline(std::cin, phonebook->contact[phonebook->counter % 8].DarkestSecret);
+	getInput("Enter first name: ", phonebook->contact[phonebook->counter % 8].FirstName);
+	getInput("Enter last name: ", phonebook->contact[phonebook->counter % 8].LastName);
+	getInput("Enter nickname: ", phonebook->contact[phonebook->counter % 8].Nickname);
+	getInput("Enter phone number: ", phonebook->contact[phonebook->counter % 8].PhoneNumber);
+	getInput("Enter darkest secret: ", phonebook->contact[phonebook->counter % 8].DarkestSecret);
 	phonebook->counter++;
 }
 
@@ -99,9 +106,10 @@ void	ft_search(class PhoneBook *phonebook)
 	{
 		ft_cout("Enter the index number for user's contact info: ", 0);
 		std::cin >> i;
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		if (std::cin.fail()) {
-            std::cin.clear(); // Clears the error state
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Remove bad input
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             ft_cout("Please enter a valid numeric index.", 1);
         } 
 		else if (i >= 0 && i < 8 && i < phonebook->counter)
@@ -117,12 +125,13 @@ int	main(void)
 	PhoneBook	phonebook(0);
 	std::string	userInput;
 
-	while (1)
+	while (true)
 	{
 		ft_cout("Please Enter one of the following 3 commands: ADD, SEARCH, EXIT", 1);
 		ft_cout("Your input: ", 0);
 		std::getline(std::cin, userInput);
-		//std::cin >> userInput;
+		if (userInput.empty())
+			exit(0);
 		if (userInput.compare("ADD") == 0)
 			ft_add(&phonebook);
 		else if (userInput.compare("SEARCH") == 0)
