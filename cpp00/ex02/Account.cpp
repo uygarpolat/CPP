5fb89e3b953e193196b26c782b4b0b6849fb1701
uygarpolat <uygarpolat@gmail.com>
2024-11-18 22:20:59 +0200

    cpp00/ex02 improvements

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -10,9 +10,83 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include Accound.hpp
+#include "Account.hpp"
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+#include <ctime>
 
-static void	Account::displayAccoundInfos( void)
+
+void	Account::displayAccountsInfos( void)
 {
 	
+}
+
+void Account::_displayTimestamp(void)
+{
+    auto now = std::chrono::system_clock::now();
+    std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
+    std::tm *localTime = std::localtime(&currentTime);
+	std::cout << "[" << std::put_time(localTime, "%Y%m%d_%H%M%S") << "] ";
+}
+
+/*
+int	Account::getNbAccounts( void )
+{
+    return (_nbAccounts);
+}
+
+int	Account::getTotalAmount( void )
+{
+    return (_totalAmount);
+}
+
+int	Account::getNbDeposits( void )
+{
+    return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void )
+{
+    return (_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos( void )
+{
+
+}
+
+void	Account::makeDeposit( int deposit )
+{
+
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+
+}
+
+int		Account::checkAmount( void ) const
+{
+
+}
+
+void	Account::displayStatus( void ) const
+{
+
+}
+*/
+// Initialize static member variables
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+	: _accountIndex(_nbAccounts), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
+{
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
+	Account::_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
 }
