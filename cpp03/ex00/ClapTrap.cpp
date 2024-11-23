ee194dd82923a35796e02f05701aa4527badd9ba
uygarpolat <uygarpolat@gmail.com>
2024-11-23 16:52:28 +0200

    cpp03/ex00 finished

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,19 +6,26 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 01:15:57 by upolat            #+#    #+#             */
-/*   Updated: 2024/11/23 01:35:35 by upolat           ###   ########.fr       */
+/*   Updated: 2024/11/23 16:51:14 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+#include <iostream>
+#include <string>
 
-ClapTrap::ClapTrap() : _hitPoints(10), _energyPoints(10), _attackDamage(0) {}
+ClapTrap::ClapTrap() : _name("Unknown name"), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
+    std::cout << "Default constructor is called for ClapTrap " << _name << std::endl;
+}
 
 ClapTrap::ClapTrap(const ClapTrap &other)
     : _name(other._name), _hitPoints(other._hitPoints), _energyPoints(other._energyPoints),
-    _attackDamage(other._attackDamage) {}
+    _attackDamage(other._attackDamage) {
+     std::cout << "Copy constructor called" << std::endl;
+     }
 
 ClapTrap &ClapTrap::operator=(const ClapTrap &other) {
+    std::cout << "Copy assignment operator called" << std::endl;
     if (this != &other) {
         _name = other._name;
         _hitPoints = other._hitPoints;
@@ -28,19 +35,57 @@ ClapTrap &ClapTrap::operator=(const ClapTrap &other) {
     return *this;
 }
 
-ClapTrap::~ClapTrap() {}
+ClapTrap::~ClapTrap() {
+    std::cout << "Destructor is called for " << _name << std::endl;
+}
 
 ClapTrap::ClapTrap(std::string name)
-    : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0) {}
+    : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
+        std::cout << "Parametrized constructor is called for " << _name << std::endl;
+    }
 
 void ClapTrap::attack(const std::string& target) {
-    (void)target;
+    std::cout << "ClapTrap " << _name
+                << " attacks " << target;
+    if (!_hitPoints)
+        std::cout << ", alas it doesn't have Hit Points, so nothing happens!" << std::endl;
+    else if (!_energyPoints)
+        std::cout << ", alas it doesn't have Energy Points, so nothing happens!" << std::endl;
+    else
+    {
+        std::cout << ", causing " << _attackDamage << " points of damage!" << std::endl;
+        _energyPoints--;
+        // other.takeDamage(_attackDamage);
+    }
 }
 
 void ClapTrap::takeDamage(unsigned int amount) {
-    (void)amount;
+    if (!_hitPoints || !_energyPoints) {
+        std::cout << "ClapTrap " << _name << " cannot take any more damage!" << std::endl;
+        _hitPoints -= amount;
+        if (_hitPoints < 0)
+            _hitPoints = 0;
+    }
+    else {
+        _hitPoints -= amount;
+        if (_hitPoints < 0)
+            _hitPoints = 0;
+        std::cout << "ClapTrap " << _name << " takes " << amount << " damage, now it has " << _hitPoints << " Hit Points!" << std::endl;
+    }
 }
 
 void ClapTrap::beRepaired(unsigned int amount) {
-    (void)amount;
+    std::cout << "ClapTrap " << _name
+                << " tries healing itself";
+    if (!_hitPoints)
+        std::cout << ", alas it doesn't have Hit Points, so nothing happens!" << std::endl;
+    else if (!_energyPoints)
+        std::cout << ", alas it doesn't have Energy Points, so nothing happens!" << std::endl;
+    else
+    {
+        std::cout << ", and succeeds! It got " << amount << " Hit Points back";
+        _energyPoints--;
+        _hitPoints += amount;
+        std::cout << ", now it has " << _hitPoints << std::endl;;
+    }
 }
--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,13 +6,56 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 01:16:12 by upolat            #+#    #+#             */
-/*   Updated: 2024/11/23 01:17:23 by upolat           ###   ########.fr       */
+/*   Updated: 2024/11/23 16:36:00 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
+int	main(void)
+{
+	ClapTrap	vannila;
+	ClapTrap	uno("'What is love?'");
+	ClapTrap	duo("'Baby dont hurt me!'");
+
+	uno.attack("'Baby dont hurt me!'");
+	duo.takeDamage(0);
+	duo.beRepaired(9999);
+	
+	ClapTrap	thisOneIsAngry(duo);
+
+	thisOneIsAngry.attack("'What is love?'");
+	uno.takeDamage(10);
+	uno.beRepaired(10);
+
+	for (int i = 0; i < 10; i++)
+		thisOneIsAngry.attack("'What is love?'");
+
+	uno.takeDamage(1);
+	thisOneIsAngry.beRepaired(1);
+	return 0;
+}
+
+
+/*
 int main(void) {
     
+    ClapTrap beast("Beast");
+
+    beast.attack("The Punisher");
+    beast.attack("The Punisher");
+
+    beast.attack("The Punisher");
+    beast.attack("The Punisher");
+    beast.attack("The Punisher");
+    beast.attack("The Punisher");
+    beast.attack("The Punisher");
+    beast.attack("The Punisher");
+    beast.beRepaired(3);
+    beast.takeDamage(13);
+    beast.takeDamage(12);
+    beast.beRepaired(3);
+    beast.beRepaired(3);
     return 0;
 }
+*/
