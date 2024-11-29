82cd2f28fe07ab13ec380f93174c43464ab400a6
Uygar Polat <upolat@c2r5p7.hive.fi>
2024-11-29 12:04:26 +0200

    added more tests for cpp05/ex03

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -17,14 +17,52 @@
 #include "Intern.hpp"
 
 int main(void) {
+
+    std::cout << " *****   TEST 1   ***** " << std::endl;
+    // Trying to create a valid form
     try {
         Intern someRandomIntern;
         AForm* rrf;
-        rrf = someRandomIntern.makeForm("shrubbery creation", "Bender");
+        rrf = someRandomIntern.makeForm("shrubbery creation", "Zoidberg");
         std::cout << *rrf << std::endl;
         delete rrf;
     }
     catch (std::exception &e) {
         std::cout << e.what();
     }
+
+    std::cout << " *****   TEST 2   ***** " << std::endl;
+    // Trying to create an invalid form
+    try {
+        Intern someRandomIntern;
+        AForm* rrf;
+        rrf = someRandomIntern.makeForm("vice presidential pardon", "Leila");
+        std::cout << *rrf << std::endl;
+        delete rrf;
+    }
+    catch (std::exception &e) {
+        std::cout << e.what();
+    }
+
+    std::cout << " *****   TEST 3   ***** " << std::endl;
+    // Creating a valid form and then executing it
+    try {
+        Intern someRandomIntern;
+        AForm* rrf;
+        rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+
+        Bureaucrat hermes(5, "Hermes");
+        Bureaucrat fry(70, "Fry");
+
+        fry.signForm(*rrf); // Fry will sign the form
+        fry.executeForm(*rrf); // Fry won't be able to execute the form (Grade too low)
+        hermes.signForm(*rrf); // Hermes won't be able to sign the form (alredy signed)
+        hermes.executeForm(*rrf); // Hermes will execute the form
+
+        delete rrf;
+    }
+    catch (std::exception &e) {
+        std::cout << e.what();
+    }
+    return 0;
 }
