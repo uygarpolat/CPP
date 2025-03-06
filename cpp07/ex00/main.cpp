58ef4a2e1e54f548d333d360bb131edfb47e0a28
uygarpolat <uygarpolat@gmail.com>
2025-03-06 10:27:14 +0200

    more tests added to cpp07/ex00

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 10:06:52 by upolat            #+#    #+#             */
-/*   Updated: 2025/03/06 10:17:45 by upolat           ###   ########.fr       */
+/*   Updated: 2025/03/06 10:25:22 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,32 @@
 #include "iostream"
 
 int main(void) {
-	std::cout << max<int>(3,5) << std::endl;
-	std::cout << max<double>(42.4242, 42.5252) << std::endl;
-	std::cout << min<char>('a', 'b') << std::endl;
+	std::cout << max(3,5) << std::endl;
+	std::cout << max(42.4242, 42.5252) << std::endl;
+	std::cout << min('a', 'b') << std::endl;
 	std::cout << min<int>('a', 'b') << std::endl;
 
 	int first = -100;
 	int second = 100;
 	std::cout << first << std::endl;
 	std::cout << second << std::endl;
-	swap<int>(first, second);
+	swap(first, second);
 	std::cout << first << std::endl;
 	std::cout << second << std::endl;
+
+	std::cout << std::endl << "Tests from the subject:" << std::endl << std::endl;
+
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	return 0;
 }
