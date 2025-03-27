5df5a7c84251622ba4f1ad090dd52b2001fda93c
Uygar Polat <upolat@c2r4p11.hive.fi>
2025-03-27 12:25:02 +0200

    error handling improvements to main function

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 09:57:51 by upolat            #+#    #+#             */
-/*   Updated: 2025/03/01 22:26:06 by upolat           ###   ########.fr       */
+/*   Updated: 2025/03/27 12:23:35 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,42 +61,63 @@ int main(void) {
     
     std::cout << "***** TEST 5 *****" << std::endl;
     // Testing << overload operator
-    Form form5("1099", 100, 50);
-    std::cout << form5 << std::endl;
+	try {
+		Form form5("1099", 100, 50);
+		std::cout << form5 << std::endl;
+	}
+	catch (std::exception &e) {
+        std::cout << e.what();
+    }
+	
 
     /* --------------------------- */
     
     std::cout << "***** TEST 6 *****" << std::endl;
     // Performing regular form signing operations. No exceptions expected in the main function.
-    Form form6("Lomake", 1, 40);
-    std::cout << form6 << std::endl;
-    Bureaucrat bur39(39, "Bureaucrat-39");
-    Bureaucrat bur41(41, "Bureaucrat-41");
-    std::cout << bur39 << std::endl;
-    std::cout << bur41 << std::endl;
-    bur41.signForm(form6);
-    bur39.signForm(form6);
-    bur39.signForm(form6);
-    std::cout << form6 << std::endl;
+    try {
+		Form form6("Lomake", 1, 40);
+		std::cout << form6 << std::endl;
+		Bureaucrat bur39(39, "Bureaucrat-39");
+		Bureaucrat bur41(41, "Bureaucrat-41");
+		std::cout << bur39 << std::endl;
+		std::cout << bur41 << std::endl;
+		bur41.signForm(form6);
+		bur39.signForm(form6);
+		bur39.signForm(form6);
+		std::cout << form6 << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what();
+	}
 
     /* --------------------------- */
 
     std::cout << "***** TEST 7 *****" << std::endl;
     // Testing copy constructor
-    Form form7("this isn't even my final FORM", 42, 24);
-    Form form8(form7);
-    std::cout << form7 << std::endl;
-    std::cout << form8 << std::endl;
+	try {
+		Form form7("this isn't even my final FORM", 42, 24);
+		Form form8(form7);
+		std::cout << form7 << std::endl;
+		std::cout << form8 << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what();
+	}
 
     /* --------------------------- */
 
     std::cout << "***** TEST 8 *****" << std::endl;
     // Testing copy assign operator. Only signature status is copied, because rest is const.
-    Form form9("THIS is my final FORM", 1, 1);
-    Form form10;
-    form10 = form9;
-    std::cout << form9 << std::endl;
-    std::cout << form10 << std::endl;
+    try {
+		Form form9("THIS is my final FORM", 1, 1);
+		Form form10;
+		form10 = form9;
+		std::cout << form9 << std::endl;
+		std::cout << form10 << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what();
+	}
 
     return 0;
 }
