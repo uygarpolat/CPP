ac5319faf11a27287eb734673e854e5dbaedafe3
uygarpolat <uygarpolat@gmail.com>
2025-04-08 00:35:44 +0300

    better template prototype for iter in cpp07/ex01

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 17:07:48 by upolat            #+#    #+#             */
-/*   Updated: 2025/04/07 17:03:58 by upolat           ###   ########.fr       */
+/*   Updated: 2025/04/08 00:26:39 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 #include "iostream"
 
-template <class T> void iter(T *arr, size_t &len, void (*func)(T &elem))
+template <class T>
+void iter(T *arr, size_t len, void (*func)(T &elem))
 {
 	for (size_t i = 0; i < len; i++)
 		(func)(arr[i]);
