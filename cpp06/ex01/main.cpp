a100a14f1724a9c4914b05e9de1bb3725bab4d1f
uygarpolat <uygarpolat@gmail.com>
2025-04-07 10:29:15 +0300

    better tests added for cpp06/ex01

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,12 +6,16 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 17:20:07 by upolat            #+#    #+#             */
-/*   Updated: 2025/03/06 09:39:55 by upolat           ###   ########.fr       */
+/*   Updated: 2025/04/07 10:28:26 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
 #include "iostream"
+#include <cassert>
+
+#define GREEN "\033[32m"
+#define DEFAULT "\033[39m"
 
 int main(void) {
 	Data* test = new Data;
@@ -20,16 +24,22 @@ int main(void) {
 	test->value = 42;
 	test->flag = true;
 
+	std::cout << GREEN << "Before serialization:" << DEFAULT << std::endl;
 	std::cout << test->str << std::endl;
 	std::cout << test->value << std::endl;
 	std::cout << test->flag << std::endl;
 
+	
 	uintptr_t raw = Serializer::serialize(test);
 	Data *test2 = Serializer::deserialize(raw);
 
+    std::cout << GREEN << "After deserialization:" << DEFAULT << std::endl;
 	std::cout << test2->str << std::endl;
 	std::cout << test2->value << std::endl;
 	std::cout << test2->flag << std::endl;
+
+	assert(test == test2);
+	std::cout << GREEN << "Assertion passed: test == test2" << DEFAULT << std::endl;
 
 	delete test;
 }
