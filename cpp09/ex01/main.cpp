ada23ce29082c44478d7e24e53004df68ccfb9ee
uygarpolat <uygarpolat@gmail.com>
2025-04-12 02:58:42 +0300

    better error handling in cpp09/ex01

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -6,23 +6,22 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 00:59:01 by upolat            #+#    #+#             */
-/*   Updated: 2025/04/12 02:00:02 by upolat           ###   ########.fr       */
+/*   Updated: 2025/04/12 02:57:47 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
 int main (int argc, char** argv) {
-	if (argc != 2) {
-		std::cerr << "Error" << std::endl;
-		return 1;
-	}
 	try {
+		if (argc != 2)
+			throw std::runtime_error("Error");
 		RPN rpn(argv[1]);
 		std::cout << rpn.parseRpn() << std::endl;
 	}
 	catch (std::exception &e) {
 		std::cerr << e.what() << std::endl;
+		return 1;
 	}
 	return 0;
 }
