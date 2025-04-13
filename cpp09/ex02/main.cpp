b9ecdffef5285769e77efdc2e8c4b24547214c95
uygarpolat <uygarpolat@gmail.com>
2025-04-14 00:52:27 +0300

    minor fixes to main.cpp

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 21:12:49 by upolat            #+#    #+#             */
-/*   Updated: 2025/04/13 20:54:40 by upolat           ###   ########.fr       */
+/*   Updated: 2025/04/14 00:50:01 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,20 @@ int main(int argc, char **argv) {
 		PmergeMe pmm(argc, argv);
 
 		std::vector<int> vec = pmm.getContainerVector();
-		std::vector<int> dq = pmm.getContainerVector();
+		std::deque<int> dq = pmm.getContainerDeque();
+		
+		clock_t start = clock();
 		pmm.insertionSortMerge(vec);
+		double diff1 = static_cast<double>(clock() - start) / CLOCKS_PER_SEC * 1000000.0;
+		
 		std::cout << "------------" << std::endl;
+		
+		start = clock();
 		pmm.insertionSortMerge(dq);
+		double diff2 = static_cast<double>(clock() - start) / CLOCKS_PER_SEC * 1000000.0;
+
+		std::cout << "Time to process a range of " << argc - 1 << " elements with std::vector : " << std::fixed << diff1 << std::endl; 
+		std::cout << "Time to process a range of " << argc - 1 << " elements with std::deque : " << std::fixed << diff2 << std::endl; 
 	}
 	catch (std::exception &e) {
 		std::cerr << e.what() << std::endl;
