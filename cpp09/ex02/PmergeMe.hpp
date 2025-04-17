de7ff2cc94e87208a491706c26b6ce4776f28ddf
uygarpolat <uygarpolat@gmail.com>
2025-04-17 16:35:51 +0300

    refactoring sorting logic of cpp09/ex02

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 21:12:11 by upolat            #+#    #+#             */
-/*   Updated: 2025/04/15 21:34:54 by upolat           ###   ########.fr       */
+/*   Updated: 2025/04/17 15:47:44 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,71 +36,103 @@ class PmergeMe {
 		// void insertLosers(T& winners, T& losers) {
 		// 	return;
 		// }
-
-		template <class T>
-		void insertionSortMerge(T &container) {
-
-			T leftover;
+		
+		// template <class T>
+		// void mergeInsertSortPairs(std::vector<std::pair<int, int>> pairs) {
 				
-			if (container.size() < 2)
-				return;
+		// 	size_t size = pairs.size();
+			
+		// 	if (size < 2)
+		// 		return;
+			
+		// 	// if size of pairs is 1, sort the pair by the bigger element which is in second position
+		// 	if (size == 2 && pairs[0].second > pairs[1].second) {
+		// 		std::swap(pairs[0], pairs[1]);
+		// 		return;
+		// 	}
+			
 	
-			std::vector<std::pair<int, int>> pairs;
-		
-			size_t size = container.size();
+		// 	T leftover;		
 			
-			for (size_t i = 0; i < size; i += 2) {
-				if (i + 1 < size) {
-					if (container[i] > container[i+1])
-						pairs.push_back(std::make_pair(container[i], container[i+1]));
-					else
-						pairs.push_back(std::make_pair(container[i+1], container[i]));
-				}
-				else
-					leftover.push_back(container[i]);
-					// pairs.push_back(std::make_pair(container[i], container[i]));
-			}
-		
-			// This sorts pairs by the first element of each pair in ascending order.
-			// The first elements will then comprise the winners container later on.
-			std::sort(pairs.begin(), pairs.end(),
-				[](const std::pair<int, int>& a,
-					const std::pair<int, int>& b) {
-					return a.first < b.first;
-				});
-		
-		
-			// specialLoser is the loser that belonged to the pair with the smallest winner
-			int specialLoser = pairs[0].second;
-		
-			T winners;
-			for (const auto &pr : pairs) {
-				winners.push_back(pr.first);
-				std::cout << "Pair: " << pr.first << "-" << pr.second << std::endl;
-			}
+		// 	for (size_t i = 0; i < size; i += 2) {
+		// 		if (i + 1 < size) {
+		// 			if (pairs[i] > pairs[i+1])
+		// 				pairs.push_back(std::make_pair(container[i], container[i+1]));
+		// 			else
+		// 				pairs.push_back(std::make_pair(container[i+1], container[i]));
+		// 		}
+		// 		else
+		// 			leftover.push_back(container[i]);
+		// 	}
 
-			return;
+		// 	mergeInsertSortPairs(pairs);
+
+		// 	return;
 		
-			insertionSortMerge(winners);
+			// // specialLoser is the loser that belonged to the pair with the smallest winner
+			// int specialLoser = pairs[0].second;
 		
-			winners.insert(winners.begin(), specialLoser);
+			// T winners;
+			// for (const auto &pr : pairs) {
+			// 	winners.push_back(pr.first);
+			// 	std::cout << "Pair: " << pr.first << "-" << pr.second << std::endl;
+			// }
+
+			// return;
 		
-			T losers;
-			// Skipping index 0, because we already added it as the specialLoser
-			for (size_t i = 1; i < pairs.size(); ++i)
-				losers.push_back(pairs[i].second);
-			if (leftover.size())
-				losers.push_back(leftover[0]);
+			// insertionSortMerge(winners);
+		
+			// winners.insert(winners.begin(), specialLoser);
+		
+			// T losers;
+			// // Skipping index 0, because we already added it as the specialLoser
+			// for (size_t i = 1; i < pairs.size(); ++i)
+			// 	losers.push_back(pairs[i].second);
+			// if (leftover.size())
+			// 	losers.push_back(leftover[0]);
 			
-			//insertLosers(winners, losers);
+			// insertLosers(winners, losers);
 		
-			container = winners;
-		}		
+			// container = winners;
+		// }
+
+		// template <class T>
+		// void mergeInsertSort(T &container) {
+			
+		// 	T leftover;
+			
+		// 	std::vector<std::pair<int, int>> pairs;
+
+		// 	size_t size = container.size();
 		
-		// 11 2 17 0 16 8 6 15 10 3 21 1 18 9 14 19 12 5 4 20 13 7
-		// Starting level == 1
-		// Level 1 index comparison: 0-1, 2-3
-		// Level 2 index comparison: 1-3, 5-7
+			
+		// 	for (size_t i = 0; i < size; i += 2) {
+		// 		if (i + 1 < size) {
+		// 			if (container[i] > container[i+1])
+		// 				pairs.push_back(std::make_pair(container[i], container[i+1]));
+		// 			else
+		// 				pairs.push_back(std::make_pair(container[i+1], container[i]));
+		// 		}
+		// 		else
+		// 			leftover.push_back(container[i]);
+		// 	}
+
+		// 	mergeInsertSortPairs(pairs);
+
+		// 	// Print pairs
+		// 	std::cout << "Pairs after sorting: ";
+		// 	for (const auto &pr : pairs) {
+		// 		std::cout << pr.first << "-" << pr.second << " ";
+		// 	}
+		// 	std::cout << std::endl;
+		// }
+
+		
+		template <class T>
+		void mergeInsertSort(T &container) {
+			pairSort(container, 1);
+		}
+	
 		template <class T>
 		void pairSort(T &container, int level) {
 			
@@ -131,5 +163,5 @@ class PmergeMe {
 			pairSort(container, level + 1);
 		}
 
-
+	
 };
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 21:12:49 by upolat            #+#    #+#             */
-/*   Updated: 2025/04/16 12:13:47 by upolat           ###   ########.fr       */
+/*   Updated: 2025/04/17 15:48:02 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,33 +20,25 @@ int main(int argc, char **argv) {
 		PmergeMe pmm(argc, argv);
 
 		std::vector<int> vec = pmm.getContainerVector();
-		std::deque<int> dq = pmm.getContainerDeque();
-
-		// for (auto v : vec)
-		// 	std::cout << v << "-";
-		// std::cout << std::endl;
-
-		// pmm.pairSort(vec, 1);
-
-		// for (auto v : vec)
-		// 	std::cout << v << "-";
-		// std::cout << std::endl;
+		//std::deque<int> dq = pmm.getContainerDeque();
 		
-
-
 		
-		clock_t start = clock();
-		pmm.pairSort(vec, 1);
-		double diff1 = static_cast<double>(clock() - start) / CLOCKS_PER_SEC * 1000000.0;
-		
-		std::cout << "------------" << std::endl;
-		
-		start = clock();
-		pmm.pairSort(dq, 1);
-		double diff2 = static_cast<double>(clock() - start) / CLOCKS_PER_SEC * 1000000.0;
+		//clock_t start = clock();
+		pmm.mergeInsertSort(vec);
+		//double diff1 = static_cast<double>(clock() - start) / CLOCKS_PER_SEC * 1000000.0;
 
-		std::cout << "Time to process a range of " << argc - 1 << " elements with std::vector : " << std::fixed << diff1 << std::endl; 
-		std::cout << "Time to process a range of " << argc - 1 << " elements with std::deque : " << std::fixed << diff2 << std::endl; 
+		for (auto v : vec)
+			std::cout << v << "-";
+		std::cout << std::endl;
+		
+		// std::cout << "------------" << std::endl;
+		
+		// start = clock();
+		// pmm.pairSort(dq, 1);
+		// double diff2 = static_cast<double>(clock() - start) / CLOCKS_PER_SEC * 1000000.0;
+
+		// std::cout << "Time to process a range of " << argc - 1 << " elements with std::vector : " << std::fixed << diff1 << std::endl; 
+		// std::cout << "Time to process a range of " << argc - 1 << " elements with std::deque : " << std::fixed << diff2 << std::endl; 
 	}
 	catch (std::exception &e) {
 		std::cerr << e.what() << std::endl;
